5e96694aa196ee1c1d50b3e5ce8170dd8db2d6f2
Hiro1006 <hota1006@gmail.com>
2022-05-22 11:54:25 +0000

    ゲストログイン機能修正

--- app/controllers/public/guests_controller.rb
+++ app/controllers/public/guests_controller.rb
@@ -2,6 +2,7 @@ class Public::GuestsController < ApplicationController
   def new_guest
    end_user = EndUser.find_or_create_by!(email: 'guest@example.com') do |end_user|
      end_user.nickname = "ゲスト"
+     end_user.password = 'password'
      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
    end
    sign_in end_user
